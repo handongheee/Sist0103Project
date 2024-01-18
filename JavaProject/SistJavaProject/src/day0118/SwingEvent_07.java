8e1fc925f44b7f71c5e1c13d77147c693d211cec
handongheee <dhhan345@gmail.com>
2024-01-18 17:45:33 +0900

    day0118

--- JavaProject/SistJavaProject/src/day0118/SwingEvent_07.java
+++ JavaProject/SistJavaProject/src/day0118/SwingEvent_07.java
@@ -3,6 +3,8 @@ package day0118;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -33,12 +35,31 @@ public class SwingEvent_07 extends JFrame {
 		
 		btn1 = new JButton("버튼1");
 		btn2 = new JButton("버튼2");
-
-		btn1.setBackground(Color.gray);
-		btn2.setBackground(Color.green);
 		
 		this.add(btn1);
 		this.add(btn2);
+		
+		// 버튼1 생성 및 이벤트 주기 (익명내부클래스)
+		btn1.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				// getContentPane().setBackground(Color.gray);
+				cp.setBackground(Color.gray);
+			}
+		});
+		
+		// 버튼2 생성 및 이벤트 주기 (익명내부클래스)
+		btn2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				cp.setBackground(Color.green);
+			}
+		});
+	
 	}
 	
 	public static void main(String[] args) {
