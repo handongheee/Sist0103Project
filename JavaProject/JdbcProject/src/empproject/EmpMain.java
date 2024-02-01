830e0d58a227553f9b453f0e9f333213956d6512
handongheee <dhhan345@gmail.com>
2024-02-01 17:47:53 +0900

    day0201

--- JavaProject/JdbcProject/src/empproject/EmpMain.java
+++ JavaProject/JdbcProject/src/empproject/EmpMain.java
@@ -1,195 +1,401 @@
 package empproject;
 
+import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Vector;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
-import day0130.AddStuInfo;
-import day0130.UpdateStuInfo;
 import oracleDB.DbConnect;
 
+//메인 인터페이스에서 클릭하면 나오는 화면
 public class EmpMain extends JFrame implements ActionListener{
 
-	DbConnect db=new DbConnect();
-	Container cp; 
+	DbConnect db = new DbConnect();
+	Container cp;
 
-	DefaultTableModel model; // 테이블에 대한 추가/삭제 시 DefaultTableModel, 불러오기만 하려면 jtable
-	JTable table; 
-	JButton btnAdd, btnDel, btnUpdate; 
+	//테이블에 대한 삭제를 하려면 DefaultTableModel이 필요하다.
+	DefaultTableModel model;
+	JTable table;
+	JButton btnAdd, btnDel, btnUpdate, btnSearch;
 
-	EmpAdd addForm=new EmpAdd("사원추가");
-	//UpdateStuInfo updateForm=new UpdateStuInfo("데이터 수정");
+	EmpAdd addForm = new EmpAdd("사원 추가");
+	EmpUpdate updateForm = new EmpUpdate("사원 수정");
 
-	public EmpMain(String title) {
-		// TODO Auto-generated constructor stub
+	// 이미지 출력위한 내부클래스
+	ImageDraw draw=new ImageDraw();
+	String imageName;
+
+
+	//생성자
+	public EmpMain(String title){
+
 		super(title);
 		cp = this.getContentPane();
-		this.setBounds(700, 400, 400, 300); // 위치x,위치y,크기x,크기y
-		cp.setBackground(new Color(255, 255, 200));
-
+		this.setBounds(700, 220, 1200, 700);
+		cp.setBackground(new Color(240,210,240));
 		initDesign();
-
-		this.setVisible(true);
+		//this.setVisible(true);
 
 	}
-	/*
+
+
+	//테이블에 DB의 데이터 전체를 출력하는 메서드
 	public void tableWrite() {
-		// 테이블 초기화
-		model.setRowCount(0); // ==> 테이블행 하나도 없다 (0행)
-		
-		// db 연결
-		Connection conn=null;
-		PreparedStatement pstmt=null;
-		ResultSet rs=null; // ResultSet: 조회/출력에서만 사용, db를 테이블형태로 가지고 오는것
-		
-		String sql="select * from emp order by emp_id";
-		
-		conn=db.getOracle();
+
+		//테이블 초기화:0을 적자. (테이블에 행 하나도 없다는 뜻)
+		model.setRowCount(0);
+
+		//db연결
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		String sql = "select * from empt order by emp_id";
+		conn = db.getOracle();
+
 		try {
-			pstmt=conn.prepareStatement(sql);
-			rs=pstmt.executeQuery(); 
-			
+
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+
 			while(rs.next()) {
-				Vector<String> data=new Vector<String>(); // 벡터: 배열은 고정형이므로 벡터/arraylist 같은 컬렉션 객체를 통해 데이터를 무한대로 추가 가능
-				// 데이터화, 벡터에 꾸러미로 묶음
-				// emp_id, emp_name, birth_date, hire_date, dept_id, position, salary, email, phone_number
-				data.add(rs.getString("emp_id")); 
+
+
+				//Q. ArrayList가 아니라 Vector를 쓰는 이유?
+				//A. 일단 연습용으로 Vector쓰는것이고, 
+				//   나중에는 더 편한 ArrayList사용한다.
+
+				Vector<String> data = new Vector<String>();
+				//전부 String 형태로 담아야한다.
+				//1.Vector로 우선 String들을 묶을 공간을 만들고,
+
+				//2.String 묶음 내에 들어갈 데이터를 하나하나 추가하기.
+				data.add(rs.getString("emp_id"));//num의 스트링화
 				data.add(rs.getString("emp_name"));
-				data.add(rs.getString("birth_date"));
-				data.add(rs.getString("hire_date"));
 				data.add(rs.getString("dept_id"));
+
+				data.add(rs.getString("birth_date"));
+				data.add(rs.getString("hire_date"));//아래 column들도 모두 String으로 묶기
+
 				data.add(rs.getString("position"));
 				data.add(rs.getString("salary"));
 				data.add(rs.getString("email"));
 				data.add(rs.getString("phone_number"));
 
-				// 테이블에 추가
+				//3.테이블에 위에 데이터들을 한꺼번에 추가
 				model.addRow(data);
+
 			}
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} finally {
+		}finally {
 			db.dbClose(rs, pstmt, conn);
 		}
 	}
-	
-	// 6. insert
-	public void insertData() {
-		// //tfName, tfBirth, tfHire, tfPosition, tfSalary, tfEmail, tfPhone
-		String name=addForm.tfName.getText(); // 이름 값 가져옴
-		String birth=addForm.tfBirth.getText();
-		String hire=addForm.tfHire.getText();
-		String position=addForm.tfPosition.getText();
-		String salary=addForm.tfSalary.getText();
-		String email=addForm.tfEmail.getText();
-		String phone=addForm.tfPhone.getText();
-		String dept=(String)addForm.cbDeptName.getSelectedItem(); // getSelectedItem: 콤보 값 가져옴
-		
 
-		
-		String sql="insert into emp values (seq_emp.nextval, ?,?,?,?,?,?,?,?)";
-		
-		// db 연결, sql전송
-		Connection conn=db.getOracle();
-		PreparedStatement pstmt=null;
-		
+
+	//insert메서드
+	public void insertData() {
+
+		// 이름, 생일,입사일,직급,월급,이메일,핸드폰, 부서명 콤보박스
+		String name = addForm.tfName.getText();
+		String dept_id = (String)addForm.cbDeptName.getSelectedItem();
+		String birth = addForm.tfBirth.getText();
+		String hire = addForm.tfHire.getText();
+		String position = addForm.tfPosition.getText();
+		String salary = addForm.tfSalary.getText();
+		String email = addForm.tfEmail.getText();
+		String phone = addForm.tfPhone.getText();
+		String image = addForm.imageName;
+
+		String sql = "insert into empt values(seq_emp.nextval,?,?,?,?,?,?,?,?,?)";
+
+		//DB연결
+		Connection conn = db.getOracle();
+		PreparedStatement pstmt = null;
+
 		try {
-			pstmt=conn.prepareStatement(sql);
-			
-			// 바인딩
+			pstmt = conn.prepareStatement(sql);
+
 			pstmt.setString(1, name);
 			pstmt.setString(2, birth);
 			pstmt.setString(3, hire);
-			pstmt.setString(4, dept);
+			pstmt.setString(4, dept_id);
 			pstmt.setString(5, position);
 			pstmt.setString(6, salary);
 			pstmt.setString(7, email);
 			pstmt.setString(8, phone);
-			
-			
-			// 업데이트
+			pstmt.setString(9, image);
+
+
+			//업데이트
 			pstmt.execute();
-			
+
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}finally {
 			db.dbClose(pstmt, conn);
-		}		
+		}
 	}
-	
 
-*/
+
+	//delete메서드
+	public void deleteData(String num) {
+		String sql = "delete from empt where emp_id ="+num;
+
+		//DB연결
+		Connection conn = db.getOracle();
+		PreparedStatement pstmt = null;
+
+		try {
+			pstmt = conn.prepareStatement(sql);
+
+			//업데이트
+			int a = pstmt.executeUpdate();
+
+			if(a==1) {
+				JOptionPane.showMessageDialog(this, "삭제가 성공했습니다.");
+				tableWrite();
+			}else {
+				JOptionPane.showMessageDialog(this, "없는 시퀀스 번호입니다.");
+			}
+
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			db.dbClose(pstmt, conn);
+		}
+	}
+
+
+	//update메서드
+	public void updateData() {
+
+		String num = updateForm.num;
+
+		String name = updateForm.tfName.getText();
+		String birth = updateForm.tfBirth.getText();
+		String hire = updateForm.tfHire.getText();
+		String position = updateForm.tfPosition.getText();
+		String salary = updateForm.tfSalary.getText();
+		String email = updateForm.tfEmail.getText();
+		String phone = updateForm.tfPhone.getText();
+		String dept_id = (String)updateForm.cbDeptName.getSelectedItem();//
+
+		String sql = "update empt set emp_name=?, birth_date=?, hire_date=?, dept_id=?, position=?, salary=?, email=?, phone_number=? where emp_id=?";
+
+		Connection conn = db.getOracle();
+		PreparedStatement pstmt = null;
+
+		try {
+
+			pstmt = conn.prepareStatement(sql);
+
+			//?차례대로 바인딩
+			pstmt.setString(1, name);
+			pstmt.setString(2, birth);
+			pstmt.setString(3, hire);
+			pstmt.setString(4, dept_id);
+			pstmt.setString(5, position);
+			pstmt.setString(6, salary);
+			pstmt.setString(7, email);
+			pstmt.setString(8, phone);
+			pstmt.setString(9, num);
+
+			//업데이트
+			pstmt.execute();
+
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}finally {
+			db.dbClose(pstmt, conn);
+		}
+
+		updateForm.setVisible(false);
+	}
+
+
+
+	// 검색메서드
+	public void searchData(String name) {
+		model.setRowCount(0);
+
+		Connection conn = db.getOracle();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		String sql = "select * from empt where emp_name like '%" + name + "%'";
+
+
+		try {
+			pstmt = conn.prepareStatement(sql);
+			//pstmt.setString(1, "%"+name+"%");
+			System.out.println(sql);
+
+			rs = pstmt.executeQuery();
+
+
+			while(rs.next()) {
+				Vector<String> data = new Vector<String>();
+
+
+				data.add(rs.getString("emp_id"));
+				data.add(rs.getString("emp_name"));
+				data.add(rs.getString("dept_id"));
+
+				data.add(rs.getString("birth_date"));
+				data.add(rs.getString("hire_date"));
+
+				data.add(rs.getString("position"));
+				data.add(rs.getString("salary"));
+				data.add(rs.getString("email"));
+				data.add(rs.getString("phone_number"));
+
+				model.addRow(data);
+
+
+			}
+
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			db.dbClose(rs, pstmt, conn);
+		}
+
+	}
+
+
+	//디자인
 	public void initDesign() {
 
-		String [] title= {"번호","이름","생일","입사일","부서","직급","월급",	"이메일","전화번호"}; // 배열선언
+		String [] title = {"사원번호", "이름", "부서명", "생년월일", "입사일","직급","월급","이메일", "휴대폰번호"};
 
-		model=new DefaultTableModel(title, 0); // 0 데이터 없음이라는 뜻
-		table=new JTable(model);
-		this.add("Center", new JScrollPane(table)); // 스크롤팬 생성 후 스크롤팬에 테이블 넣기
+		model = new DefaultTableModel(title,0);
+		table = new JTable(model);
+		this.add( "Center", new JScrollPane(table));
 
-		// 테이블에 db데이터 출력
-		//tableWrite(); // 호출
+		//테이블에 DB데이터 출력
+		tableWrite();		
 
-		// 버튼 3개 올릴 패널(공간)
-		JPanel pTop=new JPanel();
+		//버튼 4개 올릴 패널
+		JPanel pTop = new JPanel();
+		pTop.setBounds(100, 100, 1000, 100);
 		this.add("North",pTop);
 
-		// 추가버튼
-		btnAdd=new JButton("추가"); // 생성
-		//btnAdd.addActionListener(this);
-		pTop.add(btnAdd);
-		// 추가폼에 있는 추가버튼에 액션 추가 AddStuInfo
-		//addForm.btnInsert.addActionListener(this);
+		JPanel pRight = new JPanel();
+		pRight.setBounds(100, 100, 1000, 100);
+		this.add("East",pRight);
 
-		// 삭제버튼
-		btnDel=new JButton("삭제");
-		//btnDel.addActionListener(this);
+		//추가버튼
+		btnAdd = new JButton("추가");
+		btnAdd.setBounds(10, 70, 90, 30);
+		btnAdd.addActionListener(this);
+		pTop.add(btnAdd);
+		//추가폼에 있는 추가버튼에 액션을 추가
+		addForm.btnInsert.addActionListener(this);
+
+		//삭제버튼
+		btnDel = new JButton("삭제");
+		btnDel.setBounds(10, 100, 90, 30);
+		btnDel.addActionListener(this);
 		pTop.add(btnDel);
 
-		// 수정버튼
-		btnUpdate=new JButton("수정");
-		//btnUpdate.addActionListener(this);
+		//수정버튼		
+		btnUpdate = new JButton("수정");
+		btnUpdate.setBounds(10, 130, 90, 30);
+		btnUpdate.addActionListener(this);
 		pTop.add(btnUpdate);
-		// 수정폼에 있는 수정버튼에 액션 추가
-		//updateForm.btnMod.addActionListener(this);
+		//수정폼에 있는 수정버튼에 액션을 추가
+		updateForm.btnUpdate.addActionListener(this);
+
+		//검색버튼
+		btnSearch = new JButton("검색");
+		btnSearch.setBounds(10, 100, 90, 30);
+		btnSearch.addActionListener(this);
+		pTop.add(btnSearch);
+
+
+		////////////////////////////////////////////////////
+		// 이미지 위치
+		draw.setBounds(10, 50, 160, 220);
+		pRight.add(draw);
+
+		// 데이터 출력해주는 메서드
+		//this.dataWrite();
+
+		// 출력된 데이터 이벤트 (익명내부클래스)
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// 행번호 얻기
+				int row=table.getSelectedRow();
+				System.out.println(row);
+				
+				// 내가 선택한 행번호 얻기		
+				//String str="이름: "+table.getValueAt(row, 0)+", 주소: "+table.getValueAt(row, 1)+", 핸드폰: "+table.getValueAt(row, 2);		
+				//imageName =addForm.get(row).imageName;
+				
+				imageName = (String) table.getValueAt(row, 10);
+				System.out.println(imageName);
+				
+				
+				// 이미지 나오게하는 메서드 호출
+				draw.repaint();
+
+				super.mouseClicked(e);
+
+			}
+		});
 
 	}
 
+
+	//버튼 클릭하면 액션 발생시키는 메서드
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		Object ob=e.getSource();
-/*
+
+		Object ob = e.getSource();
+
 		if(ob==btnAdd) {
-			// System.out.println("add");
-			addForm.setVisible(true); // 생성한 addForm 보여지게
 
-		} else if(ob==addForm.btnInsert) { // 학생추가폼의 버튼 이벤트
-			// 입력하는 데이터를 읽어서 db에 추가
-			insertData(); // db에 들어간거 확인
+			addForm.setVisible(true);
 
-			// 테이블 다시 출력
-			tableWrite();
+		}else if(ob == addForm.btnInsert) {
 
-			// 추가창 초기화하면서 사라지게 
-			//tfName, tfBirth, tfHire, tfPosition, tfSalary, tfEmail, tfPhone
-			addForm.tfName.setText(""); // 텍스트필드 초기화
+			insertData();
+
+			this.tableWrite();
+
+			addForm.tfName.setText("");
 			addForm.tfBirth.setText("");
 			addForm.tfHire.setText("");
 			addForm.tfPosition.setText("");
@@ -197,15 +403,103 @@ public class EmpMain extends JFrame implements ActionListener{
 			addForm.tfEmail.setText("");
 			addForm.tfPhone.setText("");
 
-			addForm.setVisible(false); // 창 닫기
+			addForm.setVisible(false);
+
+		}else if(ob == btnDel) {
+
+			String num = JOptionPane.showInputDialog("삭제할 시퀀스는?");
+
+			//삭제메서드 호출
+			deleteData(num);
+
+			this.tableWrite();
+
+		}else if(ob == btnUpdate) {
+			//System.out.println("update");
+			String num = JOptionPane.showInputDialog("수정할 시퀀스는?");
+			String sql = "select * from empt where emp_id = "+num;
+
+			Connection conn = db.getOracle();
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+
+			try {
+				pstmt = conn.prepareStatement(sql);
+				rs = pstmt.executeQuery();
+
+				//데이타 하나 조회 if
+				if(rs.next()) {
+					updateForm.num = num;
+
+					updateForm.tfName.setText(rs.getString("emp_name"));
+					updateForm.tfBirth.setText(rs.getString("birth_date"));
+					updateForm.tfHire.setText(rs.getString("hire_date"));
+					updateForm.cbDeptName.setSelectedItem(rs.getString("dept_id"));
+					updateForm.tfPosition.setText(rs.getString("position"));
+					updateForm.tfSalary.setText(rs.getString("salary"));
+					updateForm.tfEmail.setText(rs.getString("email"));
+					updateForm.tfPhone.setText(rs.getString("phone_number"));
+
+
+					updateForm.setVisible(true);
+
+				}else {
+					JOptionPane.showMessageDialog(this, "존재하지 않는 데이터입니다.");
+				}
+
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}finally {
+				db.dbClose(rs, pstmt, conn);
+			}
 
 		}
-*/
+		else if(ob == updateForm.btnUpdate) {
+			updateData();
+			this.tableWrite();
+		}else if(ob==btnSearch) {
+			// 검색
+			String name = JOptionPane.showInputDialog("검색할 이름은?");
+
+			//메서드 호출
+			searchData(name);
+
+		}
+
+		//테이블 다시 출력
+		//this.tableWrite();
+
+
 	}
 
+
+	// 내부클래스
+	class ImageDraw extends Canvas{
+		@Override
+		public void paint(Graphics g) {
+			// TODO Auto-generated method stub
+			super.paint(g);
+
+			if(imageName!=null) {
+				Image image=new ImageIcon(imageName).getImage();
+				g.drawImage(image, 0, 0, 160, 200, this);
+			}
+		}
+	}
+
+
+	/*
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new EmpAdd("사원관리");
+
+
+
+		new EmpMain("★인사관리 DB★");
+
 	}
+	 */
+
+
 
 }
---
+++ JavaProject/JdbcProject/src/empproject/EmpMainDesign.java
@@ -0,0 +1,111 @@
+package empproject;
+
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import oracleDB.DbConnect;
+
+
+//제일 처음으로 나오는 화면(main메서드 여기에 존재)
+public class EmpMainDesign extends JFrame implements ActionListener{
+
+	DbConnect db=new DbConnect();
+	Container cp;
+	BufferedImage img=null;
+	
+	EmpMain em=new EmpMain("인사관리 시스템");
+	
+	JButton connbtn;
+	JLabel lblMain;
+	
+	public EmpMainDesign() {
+		setTitle("인사관리 시스템");
+		
+		JLayeredPane layeredPane=new JLayeredPane();
+		layeredPane.setSize(1200,700);
+		layeredPane.setLayout(null);
+		
+		try {
+			img=ImageIO.read(new File("C:\\sist0103\\image\\project\\emp_image.jpg"));
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "이미지 불러오기 실패");
+			System.exit(0);
+		}
+		
+		myPanel panel=new myPanel();
+		panel.setSize(1200,700);
+		layeredPane.add(panel);
+		
+		setLayout(null);
+		
+		add(layeredPane);
+		
+		initDesign();
+		
+		setBounds(700, 220,1200, 700);
+		setVisible(true);
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public void initDesign()
+	{
+		this.setLayout(null);
+		
+		
+		
+		connbtn = new JButton("실행");
+		Font customFont = new Font("바탕체",Font.BOLD, 26);
+		
+	    connbtn.setFont(customFont);
+		
+		connbtn.setBounds(550, 500, 100, 50);
+		connbtn.addActionListener(this);
+		this.add(connbtn);
+	}
+
+	
+	class myPanel extends JPanel{
+		@Override
+		public void paint(Graphics g) {
+			// TODO Auto-generated method stub
+			g.drawImage(img, 0, 0, null);
+		}
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+		Object ob = e.getSource();
+		
+		if(ob==connbtn) {
+			EmpMain add = new EmpMain("");
+			em.setVisible(true);
+			this.setVisible(false);
+			
+		}
+		
+	}
+   
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		new EmpMainDesign();
+	}
+    
+}
