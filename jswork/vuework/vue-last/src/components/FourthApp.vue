da9f24c43ad110c98cb1711c9d4b6105fcbe1f1a
HAN <dhhan345@gmail.com>
2024-02-27 16:40:35 +0900

    day0227

---
+++ jswork/vuework/vue-last/src/components/FourthApp.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1 class="alert alert-info">FourthApp 입니다</h1>
+        <div v-for="fruit in fruitArr" :key="fruit">
+            <li>{{ fruit }}</li>
+        </div>
+        <br>
+        <div v-for="info in infoArr" :key="info">
+            <li>{{ info.id + " / " + info.user }}</li>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+
+        const fruitArr = ['apple', 'banana', 'orange'];
+        const infoArr = [
+            { id: 0, user: '영수' },
+            { id: 1, user: '영식' },
+            { id: 2, user: '영철' },
+            { id: 3, user: '영희' },
+        ]
+
+        return {
+            fruitArr,
+            infoArr
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ jswork/vuework/vue-last/src/components/SecondApp.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1 class="alert alert-info">SecondApp 입니다</h1>
+        <h3>{{ cnt }}</h3>
+        <button v-on:click="cntIncre">증가</button>
+
+        <h3>{{ msg }}</h3>
+        <button @click="msgIncre">증가</button>
+
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+    setup() {
+        const cnt = ref(0);
+        const cntIncre = () => {
+            cnt.value++;
+        }
+
+        const msg = ref("안녕 뷰 재밌냐");
+        const msgIncre = () => {
+            msg.value = msg.value + "?";
+        }
+
+        return {
+            cnt,
+            cntIncre,
+            msg,
+            msgIncre
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
